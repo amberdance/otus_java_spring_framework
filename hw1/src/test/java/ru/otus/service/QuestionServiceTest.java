fc375968a79314cc8fd3d7802d1d67a404d3d27d
Alexey Fishler <amberdance@yandex.ru>
2023-09-30 15:35:12 +0300

    fix: Во избежания лишних инициализаций объектов, они перенесены в статические поля

--- hw1/src/test/java/ru/otus/service/QuestionServiceTest.java
+++ hw1/src/test/java/ru/otus/service/QuestionServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.model.Question;
+import ru.otus.repository.QuestionRepository;
 import ru.otus.repository.QuestionRepositoryImpl;
 import ru.otus.util.AbstractTest;
 import ru.otus.util.CsvQuestionMapper;
@@ -18,17 +19,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class QuestionServiceTest extends AbstractTest {
 
     private static final List<Question> dataSource = new ArrayList<>();
-    private static QuestionService questionService;
+    private static final Mapper<Question> questionMapper = new CsvQuestionMapper();
+    private static final QuestionRepository questionRepository = new QuestionRepositoryImpl(dataSource);
+    private static final QuestionService questionService = new QuestionServiceImpl(questionRepository);
 
     @BeforeEach
     void setUp() {
         dataSource.clear();
-        Mapper<Question> questionMapper = new CsvQuestionMapper();
         dataReader.read().forEach(line -> dataSource.add(questionMapper.map(line)));
-        var questionRepository = new QuestionRepositoryImpl(dataSource);
-        questionService = new QuestionServiceImpl(questionRepository);
     }
-
 
     @Test
     @DisplayName("Должен возвращать список имеющихся вопросов из источника данных")
