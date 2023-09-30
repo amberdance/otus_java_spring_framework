5bf3eaaca204bea290dcaba86d68d8caa84ea237
Alexey Fishler <amberdance@yandex.ru>
2023-09-30 13:51:24 +0300

    chore: Наброски реализации читателя csv файла

--- hw1/src/main/java/ru/otus/util/CsvDataReader.java
+++ hw1/src/main/java/ru/otus/util/CsvDataReader.java
@@ -2,15 +2,28 @@ package ru.otus.util;
 
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class CsvDataReader implements DataReader<List<String>> {
 
     private final String fileName;
 
+
     @Override
     public List<String> read() {
-        return null;
+        var contents = getFileContents();
+        return Arrays.stream(contents.split(";")).toList();
+    }
+
+    private String getFileContents() {
+        try (var resource = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            return new String(Objects.requireNonNull(resource).readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot find given file: " + fileName);
+        }
     }
 }
