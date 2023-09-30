8fe56e27f924e28abc836af9bf12a2c73db4da9a
Alexey Fishler <amberdance@yandex.ru>
2023-09-30 14:21:35 +0300

    refactor: Упрощен и исправлен CsvDataReader

--- hw1/src/main/java/ru/otus/util/CsvDataReader.java
+++ hw1/src/main/java/ru/otus/util/CsvDataReader.java
@@ -3,21 +3,26 @@ package ru.otus.util;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-public class CsvDataReader implements DataReader<List<String>> {
+public class CsvDataReader implements DataReader<List<String[]>> {
 
     private final String fileName;
 
 
     @Override
-    public List<String> read() {
-        try (var resource = getClass().getClassLoader().getResourceAsStream(fileName)) {
-            var data = Objects.requireNonNull(resource).readAllBytes();
-            return Arrays.stream(new String(data).split(";")).toList();
+    public List<String[]> read() {
+        try (InputStream resource = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            byte[] data = Objects.requireNonNull(resource).readAllBytes();
+
+            return Arrays.stream(new String(data).split("\n"))
+                    .map(s -> s.split(";"))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             throw new RuntimeException("Cannot find given file: " + fileName);
         }
